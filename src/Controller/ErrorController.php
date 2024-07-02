cffbd5ffa8c9ccd0d8bf639084bbff307a518877
sergeGuetta <ravelomahefasergegustave@gmail.com>
2024-07-02 18:07:27 +0300

    Pixelforce pour Mether

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,33 +13,33 @@ class ErrorController extends AbstractController
     {
         // Détermine le code d'état HTTP à partir de l'exception
         $code = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
-
+        throw $exception;
         // Vous pouvez personnaliser les messages d'erreur en fonction du code d'état HTTP
         switch ($code) {
-            case 401 :
+            case 401:
                 $message = 'Utilisateur non authentifié';
                 break;
-            case 403 :
+            case 403:
                 $message = 'Accès refusé';
                 break;
             case 404:
                 $message = 'Page non trouvée';
                 break;
             case 500:
-                $message ="Une erreur interne s'est produite sur le serveur.";
+                $message = "Une erreur interne s'est produite sur le serveur.";
                 break;
             case 502:
-                $message ="Une erreur interne s'est produite sur le serveur.";
+                $message = "Une erreur interne s'est produite sur le serveur.";
                 break;
             case 503:
-                $message ="Une erreur interne s'est produite sur le serveur.";
+                $message = "Une erreur interne s'est produite sur le serveur.";
                 break;
             case 504:
-                $message ="Le serveur n'a pas répondu.";
+                $message = "Le serveur n'a pas répondu.";
                 break;
-          
+
             default:
-                $message ="Une erreur interne s'est produite";
+                $message = "Une erreur interne s'est produite";
                 break;
         }
 
