391e69f23badd7e485df00c3a08ff37b6a4aae88
PrincieRakotoarivony <60096572+PrincieRakotoarivony@users.noreply.github.com>
2024-12-05 19:49:35 +0300

    changes

--- src/Form/AccountAgentType.php
+++ src/Form/AccountAgentType.php
@@ -3,15 +3,21 @@
 
 namespace App\Form;
 
+use App\Services\CountryService;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotNull;
 
 class AccountAgentType extends AbstractType
 {
+    public function __construct(private CountryService $countryService)
+    {
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -20,17 +26,21 @@ class AccountAgentType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Entrer votre nom'
                 ],
-                'constraints' => [
-                    new NotNull([],'champ obligatoire')
-                ]
+                'required' => false,
+                'trim' => true,
+                // 'constraints' => [
+                //     new NotNull([],'champ obligatoire')
+                // ]
             ])
             ->add('prenom', TextType::class, [
                 'label' => 'Prénom',
                 'attr' => [
                     'placeholder' => 'Entrer votre prénom'
                 ],
+                'required' => true,
+                'trim' => true,
                 'constraints' => [
-                    new NotNull([],'champ obligatoire')
+                    new NotBlank([], 'Prénom obligatoire')
                 ]
             ])
 
@@ -39,34 +49,44 @@ class AccountAgentType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Votre Adresse'
                 ],
-                'constraints' => [
-                    new NotNull([],'champ obligatoire')
-                ]
+                'required' => false,
+                'trim' => true,
+                // 'constraints' => [
+                //     new NotNull([], 'champ obligatoire')
+                // ]
             ])
             ->add('telephone', TelType::class, [
                 'label' => 'Numero Téléphone',
                 'attr' => [
                     'placeholder' => 'Numéro Téléphone'
                 ],
-                'constraints' => [
-                    new NotNull([],'champ obligatoire')
-                ]
+                'required' => false,
+                'trim' => true,
+                // 'constraints' => [
+                //     new NotNull([], 'champ obligatoire')
+                // ]
             ])
             ->add('codePostal', TextType::class, [
                 'label' => 'Code postal',
                 'attr' => [
                     'placeholder' => 'Code postal'
                 ],
-                'constraints' => [
-                    new NotNull([],'champ obligatoire')
-                ]
+                'required' => false,
+                'trim' => true,
+                // 'constraints' => [
+                //     new NotNull([], 'champ obligatoire')
+                // ]
             ])
             ->add('username', TextType::class, [
-                'label' => 'Pseudo',
+                'label' => 'Identifiant',
                 'attr' => [
-                    'placeholder' => 'Entrer votre pseudo',
+                    'placeholder' => 'Entrer votre identifiant',
                     'disabled' => true
-                ]
+                ],
+                // 'required' => true,
+                // 'constraints' => [
+                //     new NotBlank([], 'Identifiant obligatoire')
+                // ]
             ])
 
             ->add('email', EmailType::class, [
@@ -75,15 +95,27 @@ class AccountAgentType extends AbstractType
                     'placeholder' => 'Adresse mail',
                     'disabled' => true
                 ],
-                'required' => false,
+                // 'required' => true,
+                // 'constraints' => [
+                //     new NotBlank([], 'Adresse email obligatoire')
+                // ]
                 // 'disabled' => true
             ])
-            ->add('secteur', SecteurChoiceType::class, [
-                'label' => false,
-                'mapped' => false,
-                'disabled' => true,
-                'required' => false,
+            ->add('countryCode', ChoiceType::class, [
+                'choices' => $this->getCountryChoices(), // Custom method to define choices
+                'label' => 'Pays',
+                // 'placeholder' => 'Choose a country', // Optional: adds a default placeholder
+                'required' => true,                 // Optional: make the field required
+                'attr' => [
+                    'placeholder' => 'Pays'
+                ],
             ])
+            // ->add('secteur', SecteurChoiceType::class, [
+            //     'label' => false,
+            //     'mapped' => false,
+            //     'disabled' => true,
+            //     'required' => false,
+            // ])
             // ->add('rib', TextType::class, [
             //     'label' => 'RIB',
             //     'attr' => [
@@ -94,5 +126,15 @@ class AccountAgentType extends AbstractType
             //     ]
             // ])
         ;
+    }
+
+    private function getCountryChoices(): array
+    {
+        $data = $this->countryService->readJson();
+        $transformed = [];
+        foreach ($data as $code => $name) {
+            $transformed[$name] = $code;
+        }
+        return $transformed;
     }
 }
