c51fd7e0c9bb63bdf6d51a54b90556bdace62049
HaryTiana <harytiana9andrianirina@gmail.com>
2025-03-28 11:22:09 +0300

    fix

--- src/Services/Stat/StatAgentService.php
+++ src/Services/Stat/StatAgentService.php
@@ -302,7 +302,7 @@ class StatAgentService
         if(!$rankInfo){
             return [
                 'rank' => 1,
-                'rankName' => '',
+                'rankName' => "Apporteur d'affaire",
             ];
         }
         return [
