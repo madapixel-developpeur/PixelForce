f3d3be541a114d70f6c06bd5e115021468bba43a
PrincieRakotoarivony <60096572+PrincieRakotoarivony@users.noreply.github.com>
2025-04-28 19:31:12 +0300

    Update DocumentService.php

--- src/Services/DocumentService.php
+++ src/Services/DocumentService.php
@@ -145,7 +145,7 @@ class DocumentService
     public function getData($filepath)
     {
         $pdf = new Pdf();
-        $pdf->addFile($this->filesDirectory . '/' . $filepath, '');
+        $pdf->addFile($this->filesDirectory . '/' . $filepath, '', 'A');
         $result = $pdf->getDataFields();
 
         if ($result === false) {
